a1889c66e90e26ab94953cde4216c3b0e3465f4d
David Weinman <david@weinman.com>
2014-09-02 17:05:13 -0700

    fixed equal.py, and scoring if / else block
    re-ordered the scoring priority to reflect something that i thought
    would work properly.
    escaped output condition to grab errors in get attacker input script

--- src/equal.py
+++ src/equal.py
@@ -19,5 +19,10 @@ import sys
 def equal_precise(n1, n2, p):
 	return abs(n1 - n2) < 10 ** (-p)
 
-if __name__ == "__main__": print("%s" % str(equal_precise(float(sys.argv[1]), float(sys.argv[2]), 6)))
+if __name__ == "__main__":
+
+	try:
+		print("%s" % str(equal_precise(float(sys.argv[1]), float(sys.argv[2]), 6)))
+	except ValueError:
+		print("False")
 
